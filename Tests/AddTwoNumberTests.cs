0796018c15eb3265d742fa0ec0d8d3c62fc37d0c
ashokmazumder <ashok.mazumder@gmail.com>
2014-12-27 03:08:14 +0530

    Update AddTwoNumberTests.cs

--- Tests/AddTwoNumberTests.cs
+++ Tests/AddTwoNumberTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestFramework;
 
@@ -8,30 +8,82 @@ namespace Tests
     public class AddTwoNumberTests: TestSetup
     {
         [TestMethod]
-        public void AddTwoInteger()
+        public void AddTwoPositiveIntegerResultsPositiveNum()
         {
-            Calculator.AdditionPage.GetBrowser("Firefox");
-            Calculator.AdditionPage.AddTwoIntegerNumber(2, 3);
-            Calculator.AdditionPage.LoadUrl();
-            Assert.AreEqual(Calculator.AdditionPage.GetResult(), Convert.ToString(5));
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoIntegerNumber(2, 3);
+            Assert.AreEqual(Calculator.GetResult(), Convert.ToString(5));
         }
 
         [TestMethod]
-        public void AddTwoFloatNumber()
+        public void AddPositiveAndNegativeIntNumberResultsPositiveNumber()
         {
-            Calculator.AdditionPage.GetBrowser("Firefox");
-            Calculator.AdditionPage.AddTwoFloatNum(1 / 2, 2 / 3);
-            Calculator.AdditionPage.LoadUrl();
-            Calculator.AdditionPage.GetResult();
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoIntegerNumber(-2, 3);
+            Assert.AreEqual(Convert.ToString(1), Calculator.GetResult());
         }
 
+        //This test will fail as there is a bug in the software
+        [TestMethod]
+        public void AddPositiveAndNegativeIntNumberResultsNegativeNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoIntegerNumber(-3, 2);
+            Calculator.LoadUrlToWebBrowser();
+            Assert.AreEqual(Convert.ToString(-1), Calculator.GetResult());
+        }
+
+        //This test will fail as there is a bug in the software
+        [TestMethod]
+        public void AddTwoNegativeIntNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoIntegerNumber(-3, -2);
+            Assert.AreEqual(Convert.ToString(-5), Calculator.GetResult());
+        }
+
+        
         [TestMethod]
         public void AddTwoDoubleNumber()
         {
-            Calculator.AdditionPage.GetBrowser("Firefox");
-            Calculator.AdditionPage.AddTwoDoubleNumber(2.5, 7.5);
-            Calculator.AdditionPage.LoadUrl();
-            Assert.AreEqual(Calculator.AdditionPage.GetResult(), Convert.ToString(10));
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoDoubleNumber(2.5, 7.5);
+            Assert.AreEqual(Convert.ToString(10), Calculator.GetResult());
+        }
+
+        [TestMethod]
+        public void AddPositiveAndNegativeDoubleNumberResultsPositiveDoubleNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoDoubleNumber(10.25, -3.15);
+            Assert.AreEqual(Convert.ToString(7.10), Calculator.GetResult());
+        }
+
+        //This test will fail as there is a bug in the software
+        [TestMethod]
+        public void AddPositiveAndNegativeDoubleNumberResultsNegativeDoubleNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoDoubleNumber(-10.25, 0.24);
+            Assert.AreEqual(Convert.ToString(-10.01), Calculator.GetResult());
+        }
+
+        //This test will fail as there is a bug in the software
+        [TestMethod]
+        public void AddTwoNegativeDoubleNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoDoubleNumber(-3.33, -2.22);
+            Assert.AreEqual(Convert.ToString(-5.55), Calculator.GetResult());
+        }
+
+        //Add real numbers
+        [TestMethod]
+        public void AddTwoFloatNumber()
+        {
+            Calculator.GetBrowser("Firefox");
+            Calculator.AdditionOperation.AddTwoFloatNum(1/3, 2/3);
+            Assert.AreEqual(Convert.ToString(1), Calculator.GetResult());
         }
     }
 }
