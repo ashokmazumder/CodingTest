44b9d862e5530e7c473e1d7f0422cd1153c77751
ashokmazumder <ashok.mazumder@gmail.com>
2014-12-26 23:01:46 +0530

    Create RunTests.bat

---
+++ Scripts/RunTests.bat
@@ -0,0 +1,7 @@
+SET MSTEST_EXE_PATH="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE"
+
+REM Delete Test Result file of previous run from the workspace
+DEL %WORKSPACE%\CodingTestResults.trx 
+
+REM Run all tests
+%MSTEST_EXE_PATH%\MSTest.exe /testcontainer:%WORKSPACE%\Tests\bin\Debug\Tests.dll /resultsfile:%WORKSPACE%\CodingTestResults.trx 
