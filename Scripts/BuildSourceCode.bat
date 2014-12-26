8384148e53e1bfdebb08d33ed4810a15e206c4ab
ashokmazumder <ashok.mazumder@gmail.com>
2014-12-26 22:52:10 +0530

    added build script

--- Scripts/BuildSourceCode.bat
+++ Scripts/BuildSourceCode.bat
@@ -1,4 +1,8 @@
 REM Set the visual studio environment and build the TestSolution. 
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
-SET WORKSPACE="C:\Users\ashok.majumder\Documents"
-msbuild.exe "%WORKSPACE%\CodingTest\TestFramework\TestFramework.sln" /t:Rebuild /p:Configuration=Release /m
+REM Build Solution
+msbuild.exe "%WORKSPACE%\TestFramework\TestFramework.sln" /t:Rebuild /p:Configuration=Release /m
+
+REM Copy browser driver to bin directory
+xcopy.exe "%WORKSPACE%\Resources\chromedriver.exe" "%WORKSPACE%\Tests\bin\Debug\" /E /I /Y
+xcopy.exe "%WORKSPACE%\Resources\IEDriverServer.exe" "%WORKSPACE%\Tests\bin\Debug\" /E /I /Y
